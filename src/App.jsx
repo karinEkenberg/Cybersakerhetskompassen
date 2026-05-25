ec6fffc753087b1e62f26822f4ecb8fc2a71a8c9
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2026-05-25 13:58:21 +0200

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Hero />
+      <Footer />
     </>
   );
 }
